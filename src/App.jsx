ce7e76656c37062df4cd5f6ddcf45f2ab106623d
aigerim055 <omurzakova727@gmail.com>
2023-06-18 12:23:28 +0600

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import SignInPage from './pages/SignIn';
 import {useDispatch} from "react-redux";
 import {getImportantNews, getNews} from "./redux/thunk/thunk";
 import PostPage from "./pages/postPage";
+import EventsPage from "./pages/eventsPage";
 
 const App = () => {
 
@@ -23,6 +24,10 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path={'/'} element={<HomePage/>}/>
+                    <Route path={'/events'} element={<EventsPage/>}/>
+                    <Route path={'/reports'} element={<EventsPage/>}/>
+                    <Route path={'/shedule'} element={<EventsPage/>}/>
+                    <Route path={'/favorites'} element={<EventsPage/>}/>
                     <Route path={'/sign-up'} element={<SignUp/>}/>
                     <Route path={'/sign-in'} element={<SignInPage/>}/>
                     <Route path={'/post/:slug'} element={<PostPage/>}/>
